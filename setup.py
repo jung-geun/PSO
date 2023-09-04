fe209ce8f5649fa8e37afae375cf6681fa197a08
jung-geun <jgbong0306@gmail.com>
2023-09-04 15:05:56 +0900

    23-09-04 pypi 패키지 배포 테스트 3

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "pandas",
         "ipython",
         "tensorflow<=2.11.1",
+        "tensorboard"
     ],
     packages=find_packages(exclude=[]),
     keywords=["pso", "tensorflow", "keras"],
